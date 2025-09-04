5fc392e18ba89b9508ec99bf472ca413d6b8db8c
Shisham Chudal <shishamchudal61@gmail.com>
2025-09-04 14:05:56 -0700

    Added an event

--- demo3.js
+++ demo3.js
@@ -1,5 +1,11 @@
 //functional example and adding Events
-
-  
+function clickButton() {
+  alert("Clicked!");
+}
+  const myelement = (
+    <button type="button" onClick={clickButton}>
+      Click the button!
+    </button>
+  );
   
   ReactDOM.render(myelement, document.getElementById('root'));
